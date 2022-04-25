03aef94a86b0e3e1bac6aebe4faf5ea247191b98
0131ryu <131ryuji@gmail.com>
2022-04-25 23:40:01 +0900

    upload blog 추가 및 수정필요

--- routes/blogs/blog.ctrl.js
+++ routes/blogs/blog.ctrl.js
@@ -1,5 +1,9 @@
 "use strict";
 
+const multer = require("multer");
+const { response } = require("../../app");
+const Blog = require("../../config/blogs");
+
 const blogsList = (req, res) => {
   const blogs = [
     {
@@ -34,4 +38,47 @@ const blogsNew = (req, res) => {
   res.render("blogs/new.ejs");
 };
 
-module.exports = { blogsList, blogsNew };
+const slugFind = async (req, res) => {
+  let blog = await Blog.findOne({ slug: req.params.slug });
+
+  if (blog) {
+    res.render("show", { blog: blog });
+  } else {
+    response.redirect("/blogs");
+  }
+};
+
+const storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, "image/blogs");
+  },
+
+  //add back the extenstion
+  filename: function (req, file, callback) {
+    callback(null, Date.now() + file.originalname);
+  },
+});
+
+const uploadImage = async (req, res) => {
+  multer({
+    storage: storage,
+    limits: {
+      fieldSize: 1024 * 1024 * 3,
+    },
+  });
+  let blog = new Blog({
+    title: req.body.title,
+    author: req.body.author,
+    description: req.body.description,
+    img: req.file.filename,
+  });
+  try {
+    blog = await blog.save();
+    res.redirect(`blogs/${blog.slug}`);
+  } catch (error) {
+    console.log(error);
+  }
+  console.log(req.file);
+};
+
+module.exports = { blogsList, blogsNew, slugFind, uploadImage };
--- routes/blogs/index.js
+++ routes/blogs/index.js
@@ -6,5 +6,7 @@ const blogCtrl = require("./blog.ctrl");
 
 router.get("/blogs", blogCtrl.blogsList);
 router.get("/blogs/new", blogCtrl.blogsNew);
+router.get("/:slug", blogCtrl.slugFind);
+router.post("/blogs", upload.single("image"), blogCtrl.uploadImage);
 
 module.exports = router;
