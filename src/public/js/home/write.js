b2fc01c1f09980a8c24090faa284e1c72275eb0c
0131ryu <131ryuji@gmail.com>
2022-05-05 16:49:17 +0900

    list 관련 css 수정

---
+++ src/public/js/home/write.js
@@ -0,0 +1,23 @@
+function stayType(x) {
+  return (document.getElementById("stayTypeList").value = x);
+}
+
+function searchType_stayNum(x) {
+  return (document.getElementById("searchType_num").value = x);
+}
+
+function searchType_child(x) {
+  return (document.getElementById("searchType_childYN").value = x);
+}
+
+function searchType_senior(x) {
+  return (document.getElementById("searchType_seniorYN").value = x);
+}
+
+function searchType_bed(x) {
+  return (document.getElementById("searchType_beds").value = x);
+}
+
+function searchType_most(x) {
+  return (document.getElementById("searchType_mosts").value = x);
+}
