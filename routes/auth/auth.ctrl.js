4721df326a43270e5a0643a34d9a94c1ecf40d2e
0131ryu <131ryuji@gmail.com>
2022-05-09 00:02:17 +0900

    auth 과정

--- routes/auth/auth.ctrl.js
+++ routes/auth/auth.ctrl.js
@@ -43,6 +43,18 @@ const process = {
       });
     });
   },
+  auth: (req, res) => {
+    res.json(200).json({
+      _id: req.user._id,
+      isAdmin: req.user.role === 0 ? false : true,
+      isAuth: true,
+      email: req.user.email,
+      name: req.user.name,
+      lastname: req.user.lastname,
+      role: req.user.role,
+      image: req.user.image,
+    });
+  },
 };
 
 module.exports = {
--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const authCtrl = require("./auth.ctrl");
+const auth = require("../../src/public/js/auth");
 
-router.post("/auth/register", authCtrl.process.register);
-router.post("/auth/login", authCtrl.process.login);
-
+router.post("/api/user/register", authCtrl.process.register);
+router.post("/api/user/login", authCtrl.process.login);
+router.get("/api/user/auth", auth, authCtrl.process.auth);
 module.exports = router;
