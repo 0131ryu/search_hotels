07c7d7ccbbd02aff5e4687f90ae84294507e670d
0131ryu <131ryuji@gmail.com>
2022-05-18 16:00:06 +0900

    업로드 화면 및 Navbar 형성

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/views/Landing/LandingPage";
 import LoginPage from "./components/views/Login/LoginPage";
 import RegisterPage from "./components/views/Register/RegisterPage";
+import UploadProductPage from "./components/views/Upload/UploadProductPage";
 import Auth from "./hoc/auth";
+import NavBar from "./components/views/Navbar/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route path="/" element={Auth(LandingPage, null)} />
         <Route path="/login" element={Auth(LoginPage, false)} />
         <Route path="/register" element={Auth(RegisterPage, false)} />
+        <Route path="/product/upload" element={Auth(UploadProductPage, true)} />
       </Routes>
     </BrowserRouter>
   );
