33f155fd39a8421bc16709f1df3758475f3e3c9c
0131ryu <131ryuji@gmail.com>
2022-05-22 13:38:15 +0900

    LandingPage 관련 오류 해결

--- client/src/components/views/Landing/LandingPage.js
+++ client/src/components/views/Landing/LandingPage.js
@@ -1,35 +1,25 @@
+//LandingPage
 import React, { useEffect } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
 function LandingPage() {
-  const navigate = useNavigate();
   useEffect(() => {
-    axios.get("/api/home").then((response) => console.log(response));
-  });
-
-  const onClickHandler = () => {
-    axios.get("/api/user/logout").then((response) => {
-      // console.log(response.data);
+    axios.post("/api/product/products").then((response) => {
       if (response.data.success) {
-        navigate("/login");
+        console.log(response.data);
       } else {
-        alert("로그아웃하는데 실패했습니다.");
+        alert("상품을 가져오는데 실패했습니다.");
       }
     });
-  };
+  });
+
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "100%",
-        height: "100vh",
-      }}
-    >
-      <h2>시작페이지</h2>
-      <button onClick={onClickHandler}>로그아웃</button>
+    <div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <h1>Hello</h1>
     </div>
   );
 }
