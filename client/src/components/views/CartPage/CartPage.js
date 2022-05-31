5ce64291b753b774bd082de42571746abf32aebe
0131ryu <131ryuji@gmail.com>
2022-05-31 17:49:17 +0900

    cart페이지 관련 오류 발생

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -6,32 +6,30 @@ import {
   onSuccessBuy,
 } from "../../../_actions/user_action";
 import UserCardBlocks from "./Sections/UserCardBlocks";
-import { Empty } from "antd";
-import PayPal from "../../utils/PayPal";
+import { Empty, Result } from "antd";
+import Paypal from "../../utils/PayPal";
 
 function CartPage(props) {
   const dispatch = useDispatch();
-
   const [Total, setTotal] = useState(0);
   const [ShowTotal, setShowTotal] = useState(false);
+  const [ShowSuccess, setShowSuccess] = useState(false);
 
   useEffect(() => {
     let cartItems = [];
-    //리덕스 User state안에 cart 안에 상품이 들어있는지 확인
     if (props.user.userData && props.user.userData.cart) {
-      if (props.user.userData.cart.length > 0)
+      if (props.user.userData.cart.length > 0) {
         props.user.userData.cart.forEach((item) => {
           cartItems.push(item.id);
         });
-
-      dispatch(getCartItems(cartItems, props.user.userData.cart)).then(
-        (response) => {
-          // console.log(response);
-          if (response.payload.length > 0) {
-            calculateTotal(response.payload);
+        dispatch(getCartItems(cartItems, props.user.userData.cart)).then(
+          (response) => {
+            if (response.payload.length > 0) {
+              calculateTotal(response.payload);
+            }
           }
-        }
-      );
+        );
+      }
     }
   }, [props.user.userData]);
 
@@ -46,9 +44,8 @@ function CartPage(props) {
     setShowTotal(true);
   };
 
-  let removeFormCart = (productId) => {
+  const removeFromCart = (productId) => {
     dispatch(removeCartItem(productId)).then((response) => {
-      console.log(response);
       if (response.payload.productId.length <= 0) {
         setShowTotal(false);
       } else {
@@ -60,36 +57,66 @@ function CartPage(props) {
   const transactionSuccess = (data) => {
     dispatch(
       onSuccessBuy({
-        paymentData: data,
         cartDetail: props.user.cartDetail,
+        paymentData: data,
       })
     ).then((response) => {
       if (response.payload.success) {
+        setShowSuccess(true);
         setShowTotal(false);
       }
     });
   };
 
+  const transactionError = () => {
+    console.log("Paypal error");
+  };
+
+  const transactionCanceled = () => {
+    console.log("Transaction canceled");
+  };
+
   return (
     <div style={{ width: "85%", margin: "3rem auto" }}>
       <h1>My Cart</h1>
-
       <div>
         <UserCardBlocks
-          products={props.user.cartDetail}
-          removeItem={removeFormCart}
+          products={props.user.cart}
+          removeItem={removeFromCart}
         />
+
+        {ShowTotal ? (
+          <div style={{ marginTop: "3rem" }}>
+            <h2>Total amount: ${Total} </h2>
+          </div>
+        ) : ShowSuccess ? (
+          <Result status="success" title="Successfully Purchased Items" />
+        ) : (
+          <div
+            style={{
+              width: "100%",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+            }}
+          >
+            <br />
+            <Empty description={false} />
+            <p>No Items In the Cart</p>
+          </div>
+        )}
       </div>
 
-      {ShowTotal ? (
-        <div style={{ marginTop: "3rem" }}>
-          <h2>Total Amount: ${Total}</h2>
-        </div>
-      ) : (
-        <Empty discription={false} />
-      )}
+      {/* Paypal Button */}
 
-      {ShowTotal && <PayPal total={Total} onSuccess={transactionSuccess} />}
+      {ShowTotal && (
+        <Paypal
+          toPay={Total}
+          onSuccess={transactionSuccess}
+          transactionError={transactionError}
+          transactionCanceled={transactionCanceled}
+        />
+      )}
     </div>
   );
 }
