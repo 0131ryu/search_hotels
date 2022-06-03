dbbdb3e90795834818cc177ef26cc5e6deb5494d
0131ryu <131ryuji@gmail.com>
2022-06-04 00:05:51 +0900

    cart 부분 수정 중 - 가격 표시

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -1,10 +1,12 @@
-import React, { useEffect } from "react";
+import useItems from "antd/lib/menu/hooks/useItems";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { getCartItems } from "../../../_actions/user_action";
 import UserCardBlocks from "./Sections/UserCardBlocks";
 
 function CartPage(props) {
   const dispatch = useDispatch();
+  const [Total, setTotal] = useState(0);
 
   useEffect(() => {
     //리덕스 User 안의 cart안에 상품이 들었는지 확인
@@ -17,18 +19,31 @@ function CartPage(props) {
         });
         dispatch(getCartItems(cartItems, props.user.userData.cart)).then(
           (response) => {
-            console.log(response);
+            calculateTotal(response.payload);
           }
         );
       }
     }
   }, [props.user.userData]);
 
+  let calculateTotal = (cartDetail) => {
+    let total = 0;
+
+    cartDetail.map((item) => {
+      total += parseInt(item.price, 10) * item.quantity;
+    });
+    setTotal(total);
+  };
+
   return (
     <div style={{ width: "85%", margin: "3rem auto" }}>
       <h1>My Cart</h1>
       <div>
         <UserCardBlocks products={props.user.cartDetail} />
+      </div>
+
+      <div style={{ marginTop: "3rem" }}>
+        <h2>Total Amount : ${Total}</h2>
       </div>
     </div>
   );
